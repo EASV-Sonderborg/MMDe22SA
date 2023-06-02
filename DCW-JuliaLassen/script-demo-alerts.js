8bf71b0ad957f221f12c09c4f8f5023ffc9e5562
Aluatta <113020742+Aluatta@users.noreply.github.com>
2023-06-02 06:18:33 +0200

    demo-alrets
    js if statement

--- DCW-JuliaLassen/script-demo-alerts.js
+++ DCW-JuliaLassen/script-demo-alerts.js
@@ -1,42 +1,22 @@
 
 
-document.addEventListener("DOMContentLoaded", function() {
-        let circle = document.getElementById("circle");
-    
-        function changeColor(alertType) {
+// Function to change the color of the circle based on the alert status
+function changeColor(alertStatus) {
+    let circle = document.getElementById("circle");
+  
+    if (alertStatus === "alarm") {
+      circle.style.backgroundColor = "red";
+    } else if (alertStatus === "warning") {
+      circle.style.backgroundColor = "yellow";
+    } else {
+      circle.style.backgroundColor = "green"; //
+    }
+  }
+  
+  /** calling out function */
+   changeColor("warning");
+   changeColor("alarm"); 
 
-            switch(alertType) {
-                case "warning":
-                    circle.style.backgroundColor = "yellow";
-                    break;
-                case "alarm":
-                    circle.style.backgroundColor = "red";
-                    break;
-                case "ok":
-                    circle.style.backgroundColor = "green";
-                    break;
-                default:
-                    circle.style.backgroundColor = "gray";
-                    break;
-            }
-        }
 
-        
-  // Example alerts
-  setTimeout(function() {
-    changeColor("warning");
-    alert("Warning!");
-}, 2000);
 
-setTimeout(function() {
-    changeColor("alarm");
-    alert("Alarm!");
-}, 4000);
-
-setTimeout(function() {
-    changeColor("ok");
-    alert("OK!");
-}, 6000);
-});
-        
-    
+ 
