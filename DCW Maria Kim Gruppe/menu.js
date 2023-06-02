d8177f87c70d1be3f971300cf6f5717835ca5ad4
trixstermagee <dieselkim@hotmail.com>
2023-06-02 10:26:41 +0200

    eksamens levering
    levering af eksamens projektet til andet semester.

---
+++ DCW Maria Kim Gruppe/menu.js
@@ -0,0 +1,10 @@
+function goBack() {
+    // Handle back button functionality
+    console.log("Go back");
+  }
+  
+  function openMenu() {
+    // Handle menu button functionality
+    console.log("Open menu");
+  }
+  
