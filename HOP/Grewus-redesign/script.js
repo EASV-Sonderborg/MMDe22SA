be7e18ca5a3fd6ee4ee75fbfaa30654104b0643f
SofieHedemann <sofhedemann@gmail.com>
2024-05-20 10:13:56 +0200

    Cards components, navbar fixed on scroll, applications + trends + demo inserted on haptics

--- HOP/Grewus-redesign/script.js
+++ HOP/Grewus-redesign/script.js
@@ -17,37 +17,23 @@ function closeMenu() {
 
 
 const header = document.querySelector('header')
-const logo = document.querySelector('.nav-logo')
-const nav = document.querySelector('nav')
- 
+const logo = document.querySelector('.nav-logo');
+const nav = document.querySelector('nav'); // Add this line to select the navigation element
+
+// Get the actual height of the logo
+const logoHeight = logo.clientHeight;
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 200) {
-        setTimeout(() => {
-            logo.style.transition = 'opacity 0.5s ease-out';
-            logo.style.opacity = '0';
-        }, 100);
-        
-        // Adjust the navbar position smoothly
-        nav.style.transition = 'top 0.5s ease-out';
+        // Navbar should stick to the top
+        nav.style.position = 'fixed';
         nav.style.top = '0';
-        
-        logo.style.display = 'none';
-        logo.style.top = '-100px'
+        logo.style.opacity = '0';
     } else {
-        logo.style.display = 'block';
-        nav.style.top = ''; // Reset the top position of the nav
-
-        // Display the logo smoothly
-        setTimeout(() => {
-            logo.style.transition = 'opacity 0.5s ease-in';
-            logo.style.opacity = '1';
-        }, 100);
-
-        // Reset the navbar position smoothly
-        setTimeout(() => {
-            nav.style.transition = 'top 0.5s ease-in';
-            nav.style.top = '';
-        }, 100); // Delay the navbar transition to avoid competing with the logo transition
+        // Reset the navbar position
+        nav.style.position = 'static';
+        logo.style.opacity = '1';
+        nav.style.height = `${logoHeight}px`;
+        nav.style.height = ''; // Reset the height
     }
 });
